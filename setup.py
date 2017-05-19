5bd36cafd3608fd0f5cace93c46515c78126a31b
Jonathan Pallant <jonathan.pallant@cambridgeconsultants.com>
2017-05-19 14:32:11 +0100

    Depends on pyvmomi.
    Also, ignore build files.

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@
 from setuptools import setup
 
 setup(name='pyvmlib',
-      version='0.1',
+      version='0.2.0',
       description='A simple library for controlling VMware vCenter / ESXi servers.',
       url='http://github.com/cambridgeconsultants/pyvmlib',
       author='Cambridge Consultants',
+      author_email='jonathan.pallant@cambridgeconsultants.com',
       license='Apache-2.0',
+      install_requires=[
+        'pyvmomi',
+      ],
       packages=['pyvmlib'],
       zip_safe=False)
