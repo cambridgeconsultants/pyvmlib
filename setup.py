fe7f325b0b242f7a8206961549468a948dc74652
VickyLarmour <vicky.larmour@cambridgeconsultants.com>
2020-06-17 16:26:17 +0100

    Update setup.py
    Bump version and replace author email

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name='pyvmlib',
-      version='2.3.0',
+      version='2.4.0',
       description='A simple library for controlling VMware vSphere servers.',
       url='http://github.com/cambridgeconsultants/pyvmlib',
       author='Cambridge Consultants',
-      author_email='jonathan.pallant@cambridgeconsultants.com',
+      author_email='vicky.larmour@cambridgeconsultants.com',
       license='Apache-2.0',
       install_requires=[
         'pyvmomi',
