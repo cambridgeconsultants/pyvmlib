b4e33a63a2e655b1d1890f4e8e7ad7c83e965dd1
Finn Palmer <finn.palmer@cambridgeconsultants.com>
2021-11-16 09:52:07 +0000

    Python version / CI updates (#8)
    * Drop Python <=3.3 support (retaining 2.7 support)
    * Add newer Python build targets (3.7, 3.8, 3.9)
    * Build a wheel universal wheel output (Py2/Py3 compatible)
    * Gate deployment on passing of builds on all (supported) Python versions
    * Aesthetic updates to README (including transition to RST, preferred by PyPi)
    * Add README piping into setup.py package (so propagates into PyPi)
    * Multiple completeness updates to setup.py package info

--- setup.py
+++ setup.py
@@ -1,18 +1,46 @@
 """Setup config for pyvmlib."""
 
+import os
 from setuptools import setup
 
-setup(name='pyvmlib',
-      version='2.4.0',
-      description='A simple library for controlling VMware vSphere servers.',
-      url='http://github.com/cambridgeconsultants/pyvmlib',
-      author='Cambridge Consultants',
-      author_email='vicky.larmour@cambridgeconsultants.com',
-      license='Apache-2.0',
-      install_requires=[
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as fn:
+        return fn.read()
+
+
+setup(
+    name='pyvmlib',
+    version='2.4.1',
+    description='A simple library for controlling VMware vSphere servers.',
+    # NOTE: pypi prefers the use of RST to render docs
+    long_description=read('README.rst'),
+    url='http://github.com/cambridgeconsultants/pyvmlib',
+    author='Cambridge Consultants',
+    author_email='vicky.larmour@cambridgeconsultants.com',
+    license='Apache-2.0',
+    install_requires=[
         'pyvmomi',
         'requests',
         'six',
-      ],
-      packages=['pyvmlib'],
-      zip_safe=False)
+    ],
+    packages=['pyvmlib'],
+    classifiers=[
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'Environment :: No Input/Output (Daemon)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Distributed Computing',
+    ],
+    keywords=['pyvmlib', 'pyvmomi', 'vsphere', 'vmware', 'esxi'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    zip_safe=False
+)
